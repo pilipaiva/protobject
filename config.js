24d346f4af1ed2140d299605db9b3e31e3cd76af
pilanga <pili.paiva.2005@gmail.com>
2025-11-27 10:59:01 -0300

    estaa es

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ Protobject.setProduction(false);
 
 Protobject.initialize([
   { 
-    name: "Camara_Aruco",      // AHORA el index es la CÁMARA
+    name: "Luz_VanGogh",      // AHORA el index es la CÁMARA
     page: "index.html",
     main: true,                // El botón + aparece AQUÍ
     debug: "master",
   },
   { 
-    name: "Luz_VanGogh",       // La LUZ ahora está en camara.html
+    name: "Camara",       // La LUZ ahora está en camara.html
     page: "camara.html",
     debug: "local",
   }
